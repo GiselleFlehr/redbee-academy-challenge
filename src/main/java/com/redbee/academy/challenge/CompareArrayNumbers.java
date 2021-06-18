98df8d67b590ad4aeea08243fef35e45b410d473
Giselle Flehr <71512738+GiselleFlehr@users.noreply.github.com>
2021-06-18 18:19:28 -0300

    Update CompareArrayNumbers.java

--- src/main/java/com/redbee/academy/challenge/CompareArrayNumbers.java
+++ src/main/java/com/redbee/academy/challenge/CompareArrayNumbers.java
@@ -7,27 +7,39 @@ public class CompareArrayNumbers {
   public static void main(String[] args) {
         Scanner teclado = new Scanner (System.in);
 
-        System.out.println("Ingrese cantidad de elementos del arreglo: ");
+        System.out.println("Ingrese cantidad de elementos del arreglo 1: ");
         int n = teclado.nextInt();
-        int arreglo[] = new int [n];
-
-        for(int i=0; i<arreglo.length; i++){
+        int arreglo1[] = new int [n];
+        for(int i=0; i<arreglo1.length; i++){
             System.out.print("Ingrese elemento "+(i+1)+": ");
-            arreglo[i] = teclado.nextInt();
+            arreglo1[i] = teclado.nextInt();
         }
-    
-        int mayor;
-        mayor = arreglo[0];
-
-        for(int i=0; i<arreglo.length; i++){
-            if (arreglo[i] > mayor){
-                mayor = arreglo[i];
+        int mayor1;
+        mayor1 = arreglo1[0];
+        for(int i=0; i<arreglo1.length; i++){
+            if (arreglo1[i] > mayor1){
+                mayor1 = arreglo1[i];
             }
         }
+        System.out.println("El número mayor del arreglo 1 es: " + mayor1);
+    
+        System.out.println();
 
-        System.out.println("El número mayor del arreglo es: " + mayor);
-
+        System.out.println("Ingrese cantidad de elementos del arreglo 2: ");
+        int n = teclado.nextInt();
+        int arreglo2[] = new int [n];
+        for(int i=0; i<arreglo2.length; i++){
+            System.out.print("Ingrese elemento "+(i+1)+": ");
+            arreglo2[i] = teclado.nextInt();
         }
+        int mayor2;
+        mayor2 = arreglo2[0];
+        for(int i=0; i<arreglo2.length; i++){
+            if (arreglo2[i] > mayor2){
+                mayor2 = arreglo2[i];
+            }
+        }
+        System.out.println("El número mayor del arreglo 2 es: " + mayor2);
   
 }
 
