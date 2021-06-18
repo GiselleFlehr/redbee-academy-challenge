945b39806f993b4765e60dcc24d8db768c331971
Giselle Flehr <71512738+GiselleFlehr@users.noreply.github.com>
2021-06-18 15:43:27 -0300

    Update SumTwoNumbers.java

--- src/main/java/com/redbee/academy/challenge/SumTwoNumbers.java
+++ src/main/java/com/redbee/academy/challenge/SumTwoNumbers.java
@@ -1,17 +1,22 @@
 package com.redbee.academy.challenge;
+import java.util.Scanner;
 
 public class SumTwoNumbers {
 
-  /**
-   * Method that receives two numbers and returns the
-   * sum of both
-   *
-   * @param a - Some Integer Number
-   * @param b - Another Integer Number
-   * @return The result of a + b
-   */
-  public static Integer sum(Integer a, Integer b) {
-    //TODO: Implement me
-    return null;
-  }
+  public static void main(String[] args) {
+        int num1, num2, suma;
+
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println("Ingrese 1er número entero: ");
+        num1 = scan.nextInt();
+
+        System.out.println("Ingrese 2do número entero: ");
+        num2 = scan.nextInt();
+
+        suma = num1 + num2;
+
+        System.out.println("El resultado de la suma es: " + suma);
+
+    }
 }
